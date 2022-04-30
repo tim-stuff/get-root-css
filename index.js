87346064f65f8cc8b69a69822ea49e4eed7b4629
tim-stuff <tim.stuff.dev@gmail.com>
2022-04-30 08:32:54 -0400

    export anonymous function

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 
-module.exports.printMsg = function() {
-    console.log("This is a message from the demo package");
-}
-
-module.exports.getRootCssString = (themeObject) => {
+module.exports = (themeObject) => {
     let cssString = "";
     for (const attribute in themeObject) {
         cssString = (typeof themeObject[attribute] === "object") ? `${cssString} ${getRootCssString(themeObject[attribute])}` : `${cssString}--${attribute}:${themeObject[attribute]};`
