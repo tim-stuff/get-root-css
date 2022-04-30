e9cfab92f0878e6542add2272315e2815d377495
tim-stuff <tim.stuff.dev@gmail.com>
2022-04-30 08:26:48 -0400

    update to properly export the function

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 
-const getRootCssString = (themeObject) => {
+module.exports.printMsg = function() {
+    console.log("This is a message from the demo package");
+}
+
+module.exports.getRootCssString = (themeObject) => {
     let cssString = "";
     for (const attribute in themeObject) {
         cssString = (typeof themeObject[attribute] === "object") ? `${cssString} ${getRootCssString(themeObject[attribute])}` : `${cssString}--${attribute}:${themeObject[attribute]};`
     }
     return `:root {${cssString}}`;
 }
-
-export default getRootCssString;
